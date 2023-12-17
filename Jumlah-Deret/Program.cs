68a85a853079f6ba188bed7146fb3bed7284c5be
AmaliaPuji88 <154043597+AmaliaPuji88@users.noreply.github.com>
2023-12-17 13:43:26 +0700

    Add files via upload

---
+++ Jumlah-Deret/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Masukkan Jumlah Deret: ");
+        int jumlahDeret = Convert.ToInt32(Console.ReadLine());
+
+        // Menampilkan deret
+        for (int i = 1; i <= jumlahDeret; i++)
+        {
+            // Menampilkan angka dari 1 hingga i
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j);
+                if (j < i)
+                {
+                    Console.Write("-");
+                }
+            }
+
+            // Menambahkan '+' di akhir baris kecuali pada baris terakhir
+            if (i < jumlahDeret)
+            {
+                Console.Write("+");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
+
